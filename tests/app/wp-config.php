68f05ea07715806939bba3096c2c8bc99e82a93e
John Blackbourn <john@humanmade.com>
2021-10-31 18:38:42 +0100

    Pass the tests password through to the config.

--- tests/app/wp-config.php
+++ tests/app/wp-config.php
@@ -1,7 +1,7 @@
 <?php
 define( 'DB_NAME', 'wordpress_app' );
 define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', getenv( 'WP_TESTS_DB_PASS' ) ?: '' );
 define( 'DB_HOST', '127.0.0.1' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
